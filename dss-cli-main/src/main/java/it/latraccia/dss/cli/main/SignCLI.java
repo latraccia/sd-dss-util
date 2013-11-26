ec70cbce3d0e8f03aba256cc8b5b1fc509ef7b66
Francesco Pontillo <fpontillo@latraccia.it>
2013-11-26 13:58:15 +0100

    Set up project, add reference to DSS, handle dependencies, build
    `submods` contains the reference to the whole DSS system in a git repo (keep it up to date).
    `runConfigurations` contains the main IDEA run configurations used in the project.
    `./pom.xml` defines the modules that need to be compiled with the project and are used by the CLI.
    `./dss-cli-main/pom.xml` defines the project for the CLI and the used dependencies from the parent modules.
    `SignCLI.java` contains the `main` method for signing documents.

---
+++ dss-cli-main/src/main/java/it/latraccia/dss/cli/main/SignCLI.java
@@ -0,0 +1,16 @@
+package it.latraccia.dss.cli.main;
+
+import eu.europa.ec.markt.dss.applet.model.SignatureWizardModel;
+
+/**
+ * CLI for signing documents by using the DSS server and libraries.
+ * @author Francesco Pontillo
+ * Date: 26/11/13
+ * Time: 11.30
+ */
+public class SignCLI {
+    public static void main(String[] args) {
+        System.out.println("We're in!");
+        return;
+    }
+}
